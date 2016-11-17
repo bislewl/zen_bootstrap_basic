2e2c0777a5fc197aa0f4fab727c8736d411ca600
bislewl <bislewl@gmail.com>
2016-11-17 14:38:00 -0600

    pre-alpha

---
+++ includes/functions/extra_functions/bootstrap_basic.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *  bootstrap_basic.php
+ *
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version Author: bislewl  11/17/2016 2:23 PM Modified in zen_bootstrap_basic
+ */
